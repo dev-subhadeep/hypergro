b90ca536362704b7b5b4c1c2e14599aa25449288
Subhadeep <subhadeepsarkar1995@gmail.com>
2024-03-11 09:40:26 +0530

    feat: make pagination disabled state more obvious make disabled state translucent made the font semi-bold

--- app/src/components/Pagination.tsx
+++ app/src/components/Pagination.tsx
@@ -8,15 +8,15 @@ const Pagination = ({ pageNumber, prevPage, nextPage }: TPagination) => {
   return (
     <div className="my-4 px-4 flex justify-end items-center">
       <button
-        className="shadow-sm px-4 py-2"
+        className="shadow-sm px-4 py-2 font-semibold disabled:opacity-30"
         onClick={prevPage}
         disabled={pageNumber === 1}
       >
         Prev
       </button>
-      <span className="px-4">{pageNumber}</span>
+      <span className="px-4 font-bold">{pageNumber}</span>
       <button
-        className="shadow-sm px-4 py-2"
+        className="shadow-sm px-4 py-2 font-semibold disabled:opacity-30"
         onClick={nextPage}
         disabled={pageNumber === 10}
       >
